445a1e204ecefcd2c43dbdf45fd2073168f31c50
DESKTOP-IQE7QK3\jonas <jonasrdsantos@gmail.com>
2018-12-28 16:35:14 -0200

    File request

--- src/components/importXLS.jsx
+++ src/components/importXLS.jsx
@@ -1,7 +1,102 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-const ImportXLS = () => {
-  return <h2>ImportXLS</h2>;
-};
+class ImportXLS extends Component {
+  state = {
+    form: {
+      ship: "",
+      travel: "",
+      xls: ""
+    }
+  };
+
+  handleChange = (event, field) => {
+    let { form } = this.state;
+    form[field] = event.target.value;
+    this.setState({ form });
+  };
+
+  handleSubmit = () => {
+    // axios
+    //   .get("https://randomuser.me/api")
+    //   .then(response => (form["travel"] = JSON.stringify(response)));
+    console.log("Submit form", this.state.form);
+  };
+
+  createInput({ name, type, accept }) {
+    const { form } = this.state;
+    const fieldStyle = {
+      marginBottom: "8px",
+      width: "100%"
+    };
+    switch (type) {
+      case "file":
+        return (
+          <input
+            type={type}
+            name={name}
+            style={fieldStyle}
+            accept={accept}
+            filename={form[name]}
+            onChange={event => this.handleChange(event, name)}
+          />
+        );
+      default:
+        return (
+          <input
+            type={type}
+            name={name}
+            style={fieldStyle}
+            accept={accept}
+            value={form[name]}
+            onChange={event => this.handleChange(event, name)}
+          />
+        );
+    }
+  }
+
+  render() {
+    const formStyle = {
+      textAlign: "right",
+      width: "50%"
+    };
+    const labelStyle = {};
+    const fields = [
+      {
+        label: "Navio",
+        name: "ship",
+        type: "text"
+      },
+      {
+        label: "Viagem",
+        name: "travel",
+        type: "text"
+      },
+      {
+        label: "XLS",
+        name: "xls",
+        type: "file",
+        accept: ".xls,.xlsx"
+      }
+    ];
+    return (
+      <div className="pz-center-screen">
+        <div style={formStyle}>
+          {fields.map(field => (
+            <div className="row" key={field.name}>
+              <div className="col-3 " style={labelStyle}>
+                {field.label}
+              </div>
+              <div className="col">{this.createInput(field)}</div>
+            </div>
+          ))}
+          <button className="btn btn-primary" onClick={this.handleSubmit}>
+            Enviar
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ImportXLS;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,26 +7,13 @@ const Navbar = () => {
       <Link className="navbar-brand" to="/">
         OSBRAS
       </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <NavLink className="nav-item nav-link" to="/import-xls">
-            Importar XLS
-          </NavLink>
-          <NavLink className="nav-item nav-link" to="/consult-moves">
-            Consultar movimentos
-          </NavLink>
-        </div>
+      <div className="navbar-nav">
+        <NavLink className="nav-item nav-link" to="/import-xls">
+          Importar XLS
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/consult-moves">
+          Consultar movimentos
+        </NavLink>
       </div>
     </nav>
   );
