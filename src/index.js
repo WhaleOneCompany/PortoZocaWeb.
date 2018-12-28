c5719abc3b4fc484fa4c74c342d8b441e0feee04
DESKTOP-IQE7QK3\jonas <jonasrdsantos@gmail.com>
2018-12-28 11:36:35 -0200

    Deleted all

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Header from "./components/header";
-
-ReactDOM.render(<Header />, document.getElementById("header"));
-ReactDOM.render(<div>SHAKE YOUR BODY</div>, document.getElementById("body"));
